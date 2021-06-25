c9ad8c15eb0abe4282b45379991de0baf2cc4b83
EssienNsikak <nsikak.essienceo@gmail.com>
2021-06-25 19:57:21 +0100

    topbar fixed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import './portfolio.scss';
 
 export default function Portfolio() {
     return (
-        <div className='portfolio'>
+        <div className='portfolio' id='portfolio'>
            <h1>Portfolio</h1> 
         </div>
     )
--- src/components/topBar/Topbar.jsx
+++ src/components/topBar/Topbar.jsx
@@ -1,9 +1,32 @@
 import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className='topbar'>
-            <h1>Topbar</h1>
+        <div className={'topbar ' + (menuOpen && 'active')}>
+            <div className='wrapper'>
+
+                <div className='left'>
+                    <a href='#intro' className='logo'>genuis.</a>
+                    <div className='itemContainer'>
+                        <Person className='icon' />
+                        <span>+234 802 692 3994</span>
+                    </div>
+                    <div className='itemContainer'>
+                        <Mail className='icon' />
+                        <span>nsikak.essienceo@gmail.com</span>
+                    </div>
+                </div>
+
+                <div className='right'>
+                    <div className='hamburger' onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
